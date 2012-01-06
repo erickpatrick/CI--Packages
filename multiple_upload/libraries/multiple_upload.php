359855611606e1c575be29d1103953045f8bc74b
Erick Patrick <erickpatrick1988@gmail.com>
2012-01-06 00:12:40 -0300

    Better practices coding modifications

--- multiple_upload/libraries/multiple_upload.php
+++ multiple_upload/libraries/multiple_upload.php
@@ -2,11 +2,13 @@
 
 class Multiple_upload {
   private $CI;
-  private $config;
+  private $_config;
+  private $_data;
+  private $_errors;
 
-  private $data;
-  private $errors;
-
+  /*
+   * Initialize the multiple upload with the *basic* configuration
+   */
   public function __construct()
   {
     $this->CI = &get_instance();
@@ -14,21 +16,35 @@ class Multiple_upload {
     $this->initialize();
   }
 
+  /*
+   * When needed and if there is other config beyond *basic*, it's allowed
+   * call it by passing the config's name as the function parameter
+   */
   public function initialize($kind = 'basic')
   {
-    $this->config = $this->CI->config->item($kind);
+    $this->_config = $this->CI->config->item($kind);
   }
 
+  /*
+   * Returns all errors in the upload
+   */
   public function errors()
   {
-    return $this->errors;
+    return $this->_errors;
   }
 
+  /*
+   * Returns all data about the files after they were uploaded
+   */
   public function data()
   {
-    return $this->data;
+    return $this->_data;
   }
 
+  /*
+   * The multiple upload method. It can be used to upload single files as
+   * well as multiple files at once
+   */
   public function do_multiple_upload($field = 'userfile')
   {
     $this->CI->load->library('upload');
@@ -36,7 +52,9 @@ class Multiple_upload {
     $upl_files                = $_FILES[$field];
     $number_of_files_uploaded = count($upl_files['name']);
 
-    // Faking upload calls to $_FILE
+    // Though can be sent plenty of files, we fake as they were sent
+    // one-by-one, hacking the global $_FILES, to work with the CI's
+    // built-in Upload Library
     for ($i = 0; $i < $number_of_files_uploaded; $i++) :
       $_FILES['userfile']['name']     =     $upl_files['name'][$i];
       $_FILES['userfile']['type']     =     $upl_files['type'][$i];
@@ -45,37 +63,44 @@ class Multiple_upload {
       $_FILES['userfile']['size']     =     $upl_files['size'][$i];
 
       $config = array(
-        'file_name'     =>  $this->generate_random_name(23),
-        'allowed_types' =>   $this->config['allowed_types'],
-        'max_size'      =>  $this->config['max_files_size'],
-        'overwrite'     => $this->config['overwrite_files'],
+        'file_name'     =>  $this->_generate_random_name(23),
+        'allowed_types' =>   $this->_config['allowed_types'],
+        'max_size'      =>  $this->_config['max_files_size'],
+        'overwrite'     => $this->_config['overwrite_files'],
         
         /* real path to upload folder ALWAYS */
-        'upload_path'   => $this->config['upload_path']
+        'upload_path'   => $this->_config['upload_path']
       );
 
       $this->CI->upload->initialize($config);
 
       if ( ! $this->CI->upload->do_upload()) :
-        $this->errors[] = $this->CI->upload->display_errors();
+        $this->_errors[] = $this->CI->upload->display_errors();
       else :
-        $this->data[] = $this->CI->upload->data();
+        $this->_data[] = $this->CI->upload->data();
       endif;
     endfor;
 
-    return $this->data;
+    return $this->data();
   }
 
-  public function generate_random_name($name_length)
+  /*
+   * Generate a random name to the uploaded files with 23 chars. This is enough
+   * to guarantee no colisions will happen too soon.
+   */
+  private function _generate_random_name($name_length = 10)
   {
+    // If someone tries to lure the function
     if ($name_length <= 0) :
       return false;
     endif; 
 
     $code = "";
-    $chars = "abcdefghijklmnpqrstuvwxyzABCDEFGHIJKLMNPQRSTUVWXYZ123456789_";
-    srand((double)microtime() * 1000000);
 
+    // All valid chars to be used in generation
+    $chars = "abcdefghijklmnpqrstuvwxyzABCDEFGHIJKLMNPQRSTUVWXYZ123456789_";
+
+    srand((double)microtime() * 1000000);
     for ($i = 0; $i < $name_length; $i++) :
       $code = $code . substr($chars, rand() % strlen($chars), 1);
     endfor;
